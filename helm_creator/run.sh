617f8b16d24ed5044d8546bc2d20ae5edfa11e56
V0vaG <its_a_vio@hotmail.com>
2025-02-07 15:36:02 +0200

    auto_push

---
+++ helm_creator/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+app='topix'
+port='30084'
+
+# k3s kubectl delete -f ${app}.yaml
+
+cp manifest_template.yaml ${app}.yaml
+
+sed -i "s/APP/${app}/g" ${app}.yaml
+
+sed -i "s/PORT/${port}/g" ${app}.yaml
+
+# k3s kubectl apply -f ${app}.yaml
+
+cat ${app}.yaml | helmify ${app}
